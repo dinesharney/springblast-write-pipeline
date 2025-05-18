c7ec7428e567b6f8fd72cdb8fb40e5e7c0b9e605
Dinesh Arney <dinesharney@gmail.com>
2025-05-18 07:30:58 +0530

    added logger

--- springblast-jdbc/src/main/java/com/example/springblast/jdbc/controller/DataController.java
+++ springblast-jdbc/src/main/java/com/example/springblast/jdbc/controller/DataController.java
@@ -2,12 +2,14 @@ package com.example.springblast.jdbc.controller;
 
 import com.example.springblast.jdbc.entity.Payload;
 import com.example.springblast.jdbc.repository.PayloadRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/v1")
 public class DataController {
@@ -17,6 +19,7 @@ public class DataController {
 
     @PostMapping("/data")
     public String save(@RequestBody Payload payload) {
+        log.info("Inside Controller");
         repository.save(payload);
         return "Saved";
     }
--- springblast-r2dbc/src/main/java/com/example/springblast/r2dbc/controller/DataController.java
+++ springblast-r2dbc/src/main/java/com/example/springblast/r2dbc/controller/DataController.java
@@ -2,12 +2,14 @@ package com.example.springblast.r2dbc.controller;
 
 import com.example.springblast.r2dbc.entity.Payload;
 import com.example.springblast.r2dbc.repository.PayloadRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/v1")
 public class DataController {
@@ -20,6 +22,7 @@ public class DataController {
 
     @PostMapping("data")
     public Mono<String> save(@RequestBody Payload payload) {
+        log.info("Inside Controller");
         return repository.save(payload).thenReturn("Saved");
     }
 }
